d5e853d1b336603f07e5881ac2fcfe6e28c39918
khabab <khbabmoo599@gmail.com>
2026-05-28 16:39:20 -0400

    Add header file for notepad functions

---
+++ notepad.h
@@ -0,0 +1,10 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+#include <string>
+using namespace std;
+
+void writeFile(string filename);
+void readFile(string filename);
+void appendFile(string filename);
+
+#endif
